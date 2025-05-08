08d2789d43891ea0a472addf67a3b00365f317f5
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2025-05-08 09:25:18 -0300

    updt

--- peca/models.py
+++ peca/models.py
@@ -11,6 +11,7 @@ class Peca(models.Model):
     descricao = models.TextField(max_length=500, blank=False, null=False)
     qtd_minima = models.IntegerField(blank=True, null=True, default=0)
     unidade = models.CharField(max_length=50)
+    foto = models.ImageField(upload_to="foto/%Y/%m/%d/", blank=True)
     id_categoria_peca = models.ForeignKey(CategoriaPeca, on_delete=models.CASCADE, blank=False, null=False)
     id_local_armazenamento = models.ForeignKey(LocalArmazenamento, on_delete=models.SET_NULL, blank=True, null=True)
     id_fornecedor = models.ForeignKey(Fornecedor, on_delete=models.SET_NULL, blank=True, null=True)
--- setup/settings.py
+++ setup/settings.py
@@ -157,3 +157,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = "/media/"
+
